2c9df6858a3c7e6907f8b08493e3244950f8931b
Paul S <psolovyov@gmail.com>
2023-03-06 17:14:30 -0800

    Serializing URL and adding it to clipboard.

--- view/frontend/web/serializer.js
+++ view/frontend/web/serializer.js
@@ -1,5 +1,9 @@
 define(['jquery'], function($)
 {
+    let form = document.querySelector('#product_addtocart_form');
+    let data = new FormData(form);
+    let path = window.location.href.split('?')[0]
+
     /*!
     * Serialize all form data into an object
     * (c) 2021 Chris Ferdinandi, MIT License, https://gomakethings.com
@@ -21,10 +25,54 @@ define(['jquery'], function($)
         return obj;
     }
 
-    let form = document.querySelector('#product_addtocart_form');
-    let data = new FormData(form);
-    let serialized = serialize(data);
-    console.log(serialized);
+    // Register click event on the share button.
+    $( "#share-button").on("click", function(e)
+    {
+        e.preventDefault();
 
-    return {};    
+        let serializedString = serialize(data);
+        let parameters = $.param(serializedString,true);
+        let urlString = path + "?" + parameters;
+
+        console.log(urlString);
+        copyToClipboard(urlString);
+    });
+
+    function copyToClipboard(textToCopy) {
+        // navigator clipboard api needs a secure context (https)
+        if (navigator.clipboard && window.isSecureContext) {
+            // navigator clipboard api method'
+            return navigator.clipboard.writeText(textToCopy);
+        } else {
+            // fallback method
+            var textArea = document.createElement("textarea");
+            textArea.value = textToCopy;
+            
+            // avoid scrolling to bottom
+            textArea.style.top = "0";
+            textArea.style.left = "0";
+            textArea.style.position = "fixed";
+        
+            document.body.appendChild(textArea);
+            textArea.focus();
+            textArea.select();
+        
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Fallback: Copying text command was ' + msg);
+            } catch (err) {
+                console.error('Fallback: Oops, unable to copy', err);
+            }
+
+            document.body.removeChild(textArea);
+        }
+    }
+
+
+    // Animating button during save to clipboard process.
+    function animateShareButton()
+    {
+        console.log("animating!");
+    }
 });
